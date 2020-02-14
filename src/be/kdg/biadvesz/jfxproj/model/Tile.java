b7f4357f65f418d25c804b726810002618bec94f
Adrian <adrian.biedny@gmail.com>
2020-02-14 15:25:01 +0100

    ALLES

--- src/be/kdg/biadvesz/jfxproj/model/Tile.java
+++ src/be/kdg/biadvesz/jfxproj/model/Tile.java
@@ -11,19 +11,15 @@ public class Tile {
     public int getValue() {
         return value;
     }
-
     public int getPositionX() {
         return positionX;
     }
-
     public void setPositionX(int positionX) {
         this.positionX = positionX;
     }
-
     public int getPositionY() {
         return positionY;
     }
-
     public void setPositionY(int positionY) {
         this.positionY = positionY;
     }
