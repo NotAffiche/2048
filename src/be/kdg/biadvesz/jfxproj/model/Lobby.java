05391bfad99d455448fa05a717d1ee3ce9e9990b
szymo <szymon_vertenten@hotmail.com>
2020-02-14 16:43:30 +0100

    hihi

--- src/be/kdg/biadvesz/jfxproj/model/Lobby.java
+++ src/be/kdg/biadvesz/jfxproj/model/Lobby.java
@@ -1,10 +1,15 @@
 package be.kdg.biadvesz.jfxproj.model;
 
+import java.security.PrivateKey;
+
 public class Lobby {
     private String title;
+    private String playername;
 
-    public Lobby() {
+
+    public Lobby(String playername) {
         this.title = "2048 GAME";
+        this.playername = playername;
     }
 
 }
