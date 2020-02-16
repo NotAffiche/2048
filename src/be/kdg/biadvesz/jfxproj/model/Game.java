a4d15c81777b8b4a4876b91f6c8e9dd483d1040d
Adrian <adrian.biedny@gmail.com>
2020-02-16 17:40:02 +0100

    fixes

--- src/be/kdg/biadvesz/jfxproj/model/Game.java
+++ src/be/kdg/biadvesz/jfxproj/model/Game.java
@@ -1,19 +1,36 @@
 package be.kdg.biadvesz.jfxproj.model;
 
+import java.util.Random;
+
 public class Game {
     private int highscore;
     private int score;
     private String title;
+    private int[][] grid;
+    private int gridSize;
+    private Random rndm;
 
-
-    public void start() {
-    }
+    public int getHighscore() { return highscore; }
+    public void setHighscore(int highscore) { this.highscore = highscore; }
+    public int getScore() { return score; }
+    public void setScore(int score) { this.score = score; }
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+    public int[][] getGrid() { return grid; }
+    public void setGrid(int[][] grid) { this.grid = grid; }
+    public int getGridSize() { return this.gridSize; }
+    public void setGridSize(int gridSize) { this.gridSize = gridSize; }
 
     public Game() {
-        this.title = "2048";
-        this.score = 0;
+        rndm = new Random();
+        setGrid(new int[getGridSize()][getGridSize()]); // gridsize (configurable in future?)
+        setTitle("2048");
+        setScore(0);
         //TODO: highscore
-//        this.highscore =
+//        setHighscore();
+    }
 
+    public void start() {
+        rndm.nextInt(getGridSize());
     }
 }
--- src/be/kdg/biadvesz/jfxproj/model/Lobby.java
+++ src/be/kdg/biadvesz/jfxproj/model/Lobby.java
@@ -1,14 +1,15 @@
 package be.kdg.biadvesz.jfxproj.model;
 
-import java.security.PrivateKey;
-
 public class Lobby {
     private String title;
     private String playername;
 
-
     public Lobby(String playername) {
         this.title = "2048 GAME";
         this.playername = playername;
+    }
+
+    public void startGame(Game game) {
+        game.start();
     }
 }
