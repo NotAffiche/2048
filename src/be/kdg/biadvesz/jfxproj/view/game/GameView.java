0713142f12233326db2cad0299ea5d3722bb3a38
szymo <szymon_vertenten@hotmail.com>
2020-02-21 14:37:33 +0100

    hihi

--- src/be/kdg/biadvesz/jfxproj/view/game/GameView.java
+++ src/be/kdg/biadvesz/jfxproj/view/game/GameView.java
@@ -3,9 +3,13 @@ package be.kdg.biadvesz.jfxproj.view.game;
 import be.kdg.biadvesz.jfxproj.model.Game;
 import be.kdg.biadvesz.jfxproj.model.Tile;
 import javafx.animation.TranslateTransition;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 
-public class GameView {
+public class GameView extends BorderPane {
     private Label lblTitle;
     private Label lblScore;
     private Label lblHighscore;
@@ -17,9 +21,28 @@ public class GameView {
 
     }
     private void initialise(){
-        lblTitle = new Label()
+        lblTitle = new Label("2048 Game");
+        lblTitle.setStyle("-fx-font-size: 20px");
+        lblScore = new Label();
+        lblHighscore = new Label();
+
+    }
+    private  void layoutnodes(){
+        super.setPadding(new Insets(10));
+        super.getChildren().add(lblTitle);
+
+
     }
 
+    public Label getLblTitle() {
+        return lblTitle;
+    }
+    public Label getLblScore() {
+        return lblScore;
+    }
+    public Label getLblHighscore() {
+        return lblHighscore;
+    }
 
     private void animateUp() {
 
