45110f0732f5b3b6cbe0a54dd219aefe44af292d
szymo <szymon_vertenten@hotmail.com>
2020-02-21 14:27:50 +0100

    hihi

--- src/be/kdg/biadvesz/jfxproj/view/game/GameView.java
+++ src/be/kdg/biadvesz/jfxproj/view/game/GameView.java
@@ -1,7 +1,43 @@
 package be.kdg.biadvesz.jfxproj.view.game;
 
-import javafx.scene.layout.BorderPane;
+import be.kdg.biadvesz.jfxproj.model.Game;
+import be.kdg.biadvesz.jfxproj.model.Tile;
+import javafx.animation.TranslateTransition;
+import javafx.scene.control.Label;
 
-public class GameView extends BorderPane {
+public class GameView {
+    private Label lblTitle;
+    private Label lblScore;
+    private Label lblHighscore;
 
+
+    public GameView() {
+        initialise();
+        layoutnodes();
+
+    }
+    private void initialise(){
+        lblTitle = new Label()
+    }
+
+
+    private void animateUp() {
+
+
+    }
+
+    private void animateDown() {
+
+
+    }
+
+    private void animateLeft() {
+
+
+    }
+
+    private void animateRight() {
+
+
+    }
 }
