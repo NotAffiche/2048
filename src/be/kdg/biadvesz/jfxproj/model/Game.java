22d7afee44595e54d75c16e608eb9cd2b056661b
Adrian <adrian.biedny@gmail.com>
2020-02-25 14:57:48 +0100

    fixes + icoontje makker

--- src/be/kdg/biadvesz/jfxproj/model/Game.java
+++ src/be/kdg/biadvesz/jfxproj/model/Game.java
@@ -11,62 +11,31 @@ public class Game {
     private int gridSize;
     private Random rndm;
     private String playername;
+    private File highscoreFile;
 
-    public int getHighscore() {
-        return highscore;
-    }
-
-    public void setHighscore(int highscore) {
-        this.highscore = highscore;
-    }
-
-    public int getScore() {
-        return score;
-    }
-
-    public void setScore(int score) {
-        this.score = score;
-    }
-
-    public Tile[][] getGrid() {
-        return grid;
-    }
-
-    public void setGrid(Tile[][] grid) {
-        this.grid = grid;
-    }
-
-    public int getGridSize() {
-        return this.gridSize;
-    }
-
-    public void setGridSize(int gridSize) {
-        this.gridSize = gridSize;
-    }
-
-    public String getPlayername() {
-        return this.playername;
-    }
-
-    public void setPlayername(String playername) {
-        this.playername = playername;
-    }
+    public int getScore() { return score; }
+    public void setScore(int score) { this.score = score; }
+    public Tile[][] getGrid() { return grid; }
+    public void setGrid(Tile[][] grid) { this.grid = grid; }
+    public int getGridSize() { return this.gridSize; }
+    public void setGridSize(int gridSize) { this.gridSize = gridSize; }
+    public String getPlayername() { return this.playername; }
+    public void setPlayername(String playername) { this.playername = playername; }
 
     public Game(String playername) {
         rndm = new Random();
         setGridSize(4);
+        if (existsHighscores()) {
+            createHighscoreFile();
+        }
         setGrid(new Tile[getGridSize()][getGridSize()]);
         setScore(0);
         setPlayername(playername);
         //TODO: highscore
-        getHighscore(getPlayername());
+        startGame();
     }
 
     public void startGame() {
-        if (existsHighscores()) {
-            createHighscoreFile();
-            System.out.println("writing file");
-        }
         rndm.nextInt(getGridSize());
     }
 
@@ -74,21 +43,21 @@ public class Game {
         saveHighscore(getPlayername());
     }
 
-    private void getHighscore(String playername) {
-//        this.highscore =
-    }
-
     private void saveHighscore(String playername) {
 
     }
 
     private boolean existsHighscores() {
-        File highscoreFile = new File("/Program Files/BiadVesz/2048/highscores.txt");
-        return !highscoreFile.exists();
+        String path = System.getProperty("user.home") + File.separator + "Documents/BiadVesz";
+        File highscore = new File(path);
+        return !highscore.exists();
     }
 
     private void createHighscoreFile() {
-        File highscoreFile = new File("/Program Files/BiadVesz/2048/highscores.txt");
+        String path = System.getProperty("user.home") + File.separator + "Documents/BiadVesz/2048";
+        File folder = new File(path);
+        folder.mkdirs();
+        File highscoreFile = new File(path + "/highscores.txt");
         try {
             highscoreFile.createNewFile();
         } catch (IOException ioex) {
