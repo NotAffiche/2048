c029bc17ea95d51ff8635d4111f5fc6dc033056e
szymo <szymon_vertenten@hotmail.com>
2020-03-07 14:41:13 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	out/production/2048game/be/kdg/biadvesz/jfxproj/view/howtoplay/HowToPlayView.class
    #	out/production/2048game/be/kdg/biadvesz/jfxproj/view/lobby/LobbyView.class
    #	src/be/kdg/biadvesz/jfxproj/view/lobby/LobbyView.java

--- src/be/kdg/biadvesz/jfxproj/view/wingame/winGamePresenter.java
+++ src/be/kdg/biadvesz/jfxproj/view/wingame/winGamePresenter.java
@@ -1,17 +1,21 @@
 package be.kdg.biadvesz.jfxproj.view.wingame;
 
 import be.kdg.biadvesz.jfxproj.model.Game;
+import be.kdg.biadvesz.jfxproj.model.Lobby;
+import be.kdg.biadvesz.jfxproj.model.winGame;
 import be.kdg.biadvesz.jfxproj.view.game.GamePresenter;
 import be.kdg.biadvesz.jfxproj.view.game.GameView;
+import be.kdg.biadvesz.jfxproj.view.lobby.LobbyPresenter;
+import be.kdg.biadvesz.jfxproj.view.lobby.LobbyView;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 
 public class winGamePresenter {
     //ATTRIB
-    private winGamePresenter model;
+    private winGame model;
     private winGameView view;
 
-    public winGamePresenter(winGamePresenter model, winGameView view) {
+    public winGamePresenter(winGame model, winGameView view) {
         this.model = model;
         this.view = view;
         addEventHandlers();
@@ -22,17 +26,24 @@ public class winGamePresenter {
             @Override
             public void handle(ActionEvent actionEvent) {
 
+
             }
         });
         view.getBtExitGame().setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
+                Lobby lobbymodel = new Lobby();
+                LobbyView lobbyView = new LobbyView();
+                lobbymodel.setPlayerName(model.getPlayerName());
+                LobbyPresenter lobbyPresenter = new LobbyPresenter(lobbymodel, lobbyView);
+                view.getScene().setRoot(lobbyView);
 
             }
         });
         view.getBtRestart().setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
+
 
             }
         });
