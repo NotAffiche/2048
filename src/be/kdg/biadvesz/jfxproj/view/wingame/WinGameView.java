0de3af637aa5955051b0fa9b4d643260b0c0b5fa
szymo <szymon_vertenten@hotmail.com>
2020-03-08 12:38:50 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	out/production/2048game/be/kdg/biadvesz/jfxproj/view/howtoplay/HowToPlayView.class
    #	out/production/2048game/be/kdg/biadvesz/jfxproj/view/lobby/LobbyView.class
    #	src/be/kdg/biadvesz/jfxproj/view/lobby/LobbyView.java

--- src/be/kdg/biadvesz/jfxproj/view/wingame/WinGameView.java
+++ src/be/kdg/biadvesz/jfxproj/view/wingame/WinGameView.java
@@ -47,14 +47,7 @@ public class WinGameView extends BorderPane {
         //LABELS
         lblWon = new Label("Congratulations");
         lblText = new Label("Congratulations, you have won 2048!");
-        //IMAGES
-//        imContinue = new Image(getClass().getResourceAsStream("continue.png"));
-//        imExit = new Image(getClass().getResourceAsStream("exit.png"));
-//        imRestart = new Image(getClass().getResourceAsStream("restart.jpg"));
-//        //BUTTONS
-//        btContinue = new Button("", new ImageView(imContinue));
-//        btExitGame = new Button("", new ImageView(imExit));
-//        btRestart = new Button("", new ImageView(imRestart));
+
         btContinue = new Button("continue");
         btExitGame = new Button("exit");
         btRestart = new Button("restart");
@@ -66,44 +59,70 @@ public class WinGameView extends BorderPane {
 
         //HBOXES
         HBox hboxTitle = new HBox();
-        HBox hboxText= new HBox();
-        HBox hboxRestart = new HBox();
-        HBox hboxContinue = new HBox();
-        HBox hboxExit = new HBox();
+        HBox hboxText = new HBox();
+        HBox hbbutton1 = new HBox();
+        HBox hbbutton2 = new HBox();
+        HBox hbbutton3 = new HBox();
+
+
+        hbbutton1.setAlignment(Pos.CENTER);
+        hbbutton2.setAlignment(Pos.CENTER);
+        hbbutton3.setAlignment(Pos.CENTER);
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 
         //ADD TO HBOX
         hboxTitle.getChildren().add(lblWon);
         hboxText.getChildren().add(lblText);
-        hboxRestart.getChildren().add(btRestart);
-        hboxContinue.getChildren().add(btContinue);
-        hboxExit.getChildren().add(btExitGame);
+        hbbutton1.getChildren().add(btExitGame);
+        hbbutton2.getChildren().add(btContinue);
+        hbbutton3.getChildren().add(btRestart);
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
         //FONTS
-        lblWon.setFont(Font.font("verdana", FontWeight.NORMAL, FontPosture.REGULAR, 62));
-        lblText.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 16));
+        lblWon.setFont(Font.font("verdana", FontWeight.NORMAL, FontPosture.REGULAR, 50));
+        lblText.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 17));
+        btContinue.setFont(Font.font("verdana", FontWeight.NORMAL, FontPosture.REGULAR, 15));
+        btRestart.setFont(Font.font("verdana", FontWeight.NORMAL, FontPosture.REGULAR, 15));
+        btExitGame.setFont(Font.font("verdana", FontWeight.NORMAL, FontPosture.REGULAR, 15));
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
         //PADDINGS
-        lblText.setPadding(new Insets(40, 0, 0, 0));
-        lblWon.setPadding(new Insets(10, 0, 0, 0));
 
+        lblWon.setPadding(new Insets(40, 0, 0, 0));
+        lblText.setPadding(new Insets(0, 0, 0, 0));
 
-        //CEATE VBOX
-        VBox mainbox = new VBox();
-        mainbox.setPadding(new Insets(10));
-        mainbox.getChildren().addAll(lblWon,lblText,btRestart,btContinue,btExitGame);
+        hbbutton1.setPadding(new Insets(0, 0, 10, 0));
+        hbbutton2.setPadding(new Insets(0, 0, 10, 0));
+        hbbutton3.setPadding(new Insets(0, 0, 10, 0));
+
+        btRestart.setPadding(new Insets(12, 30, 12, 30));
+        btContinue.setPadding(new Insets(12, 30, 12, 30));
+        btExitGame.setPadding(new Insets(12, 30, 12, 30));
+
+        btExitGame.setMinWidth(175);
+        btContinue.setMinWidth(175);
+        btRestart.setMinWidth(175);
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        //VBOX
+        VBox mainBox = new VBox();
+        mainBox.setPadding(new Insets(10));
+        mainBox.getChildren().addAll(lblText);
+        VBox buttons = new VBox();
+        buttons.getChildren().addAll(hbbutton1, hbbutton2, hbbutton3);
 
         //POSITIONS
         hboxTitle.setAlignment(Pos.TOP_CENTER);
-        hboxText.setAlignment(Pos.CENTER);
-        hboxRestart.setAlignment(Pos.BOTTOM_LEFT);
-        hboxContinue.setAlignment(Pos.BOTTOM_CENTER);
-        hboxExit.setAlignment(Pos.BOTTOM_RIGHT);
+        mainBox.setAlignment(Pos.CENTER);
 
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         super.setTop(hboxTitle);
-        super.setCenter(mainbox);
-
-
+        super.setCenter(mainBox);
+        super.setBottom(buttons);
         super.setBackground(new Background(new BackgroundFill(Color.rgb(250, 248, 239), CornerRadii.EMPTY, Insets.EMPTY)));
+
 
     }
 }
